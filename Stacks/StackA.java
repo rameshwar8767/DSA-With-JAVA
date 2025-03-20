788dddd039496bed37a2e4407b11618787b5a07f
rameshwar mane <manerameshwar909@gmail.com>
2025-03-20 18:44:17 +0530

    Refactor StackA main method to implement next greater element logic

--- Stacks/StackA.java
+++ Stacks/StackA.java
@@ -21,11 +21,28 @@ public class StackA {
         }
     }
     public static void main(String[] args) {
-        int stocks [] ={100,80,60,70,60,85,100};
-        int span [] = new int[stocks.length];
-        stocksSpan(stocks, span);
-        for (int i = 0; i < span.length; i++) {
-            System.out.println(span[i]+" ");
+        // int stocks [] ={100,80,60,70,60,85,100};
+        // int span [] = new int[stocks.length];
+        // stocksSpan(stocks, span);
+        // for (int i = 0; i < span.length; i++) {
+        //     System.out.println(span[i]+" ");
+        // }
+        int arr[] = {6,8,0,1,3};
+        Stack<Integer> s = new Stack<>();
+        int nextGreater[] = new int[arr.length];
+        for(int i = arr.length-1; i>=0; i--){
+            while(!s.isEmpty() && arr[s.peek()] <= arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nextGreater[i] = -1;
+            }else{
+                nextGreater[i] = arr[s.peek()];
+            }
+            s.push(i);
+        }
+        for (int i = 0; i < nextGreater.length; i++) {
+            System.out.println(nextGreater[i]+"");
         }
     }
 }
