aaefeeaf21866648cdcafe1c76eb24f24ac9d938
rameshwar mane <manerameshwar909@gmail.com>
2025-07-04 19:53:10 +0530

    Comment out skip method call in main and add subSeq method for subsequence generation

--- Recursion/Stream.java
+++ Recursion/Stream.java
@@ -3,7 +3,8 @@ import java.util.*;
 public class Stream {
     
     public static void main(String[] args) {
-        skip("", "abcdaaahajahajky");
+        // skip("", "abcdaaahajahajky");
+        subSeq("", "abc");
     }
 
     static void skip(String p, String up){
@@ -18,5 +19,16 @@ public class Stream {
         }else{
             skip(p+ch,up.substring(1));
         }
+    }
+
+    static void subSeq(String p, String up){
+        if(up.isEmpty()){
+            System.out.println(p);
+            return;
+        }
+
+        char ch = up.charAt(0);
+        subSeq(p+ch, up.substring(1));
+        subSeq(p, up.substring(1));
     }
 }
