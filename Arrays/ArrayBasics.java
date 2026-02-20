7c996ec470eca63d2d18499b31ef06afa784c275
rameshwar mane <manerameshwar909@gmail.com>
2026-02-20 14:31:43 +0530

    Refactor and reorganize array-related classes and methods
    - Deleted obsolete classes: Array2, Arrays_Assignment, Basics, Pattern, ShortestPath, Solution, Sorting, and Str.
    - Introduced new classes: Array, Array2, Arrays_Assignment, Basics, Pattern, ShortestPath, Solution, and Sorting with updated implementations.
    - Implemented methods for array manipulation, including pair printing, subarray sums, maximum profit calculation, and rainwater trapping.
    - Added functionality for checking distinct elements and duplicates in arrays.
    - Enhanced sorting algorithms and added methods for spiral printing and staircase search in 2D arrays.
    - Updated main methods to demonstrate functionality with sample inputs.

---
+++ Arrays/ArrayBasics.java
@@ -0,0 +1,11 @@
+public class ArrayBasics {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,5};
+
+        for (int num : nums) {
+            System.out.println(num);
+        }
+        int [] arr = new int[5];
+        
+    }
+}
