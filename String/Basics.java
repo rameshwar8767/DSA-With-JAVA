a1bd5b2db0a198b722f793c21fb9780c77181bfd
rameshwar mane <manerameshwar909@gmail.com>
2026-02-24 22:41:50 +0530

    Implement Basics class with user input functionality and string output

---
+++ String/Basics.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Basics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("s");
+        String s = sc.next();
+        System.out.println(s);
+        // String s = "Rameshwar";
+        System.out.println("str");
+        String str = sc.nextLine();
+        System.out.println(str);
+    }
+}
