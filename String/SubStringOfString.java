2f39ba8853ed4343577756d8944e72a7e1371c4d
rameshwar mane <manerameshwar909@gmail.com>
2026-03-03 22:18:50 +0530

    Implement SubStringOfString class to demonstrate substring extraction from a string

---
+++ String/SubStringOfString.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class SubStringOfString {
+    public static void main(String[] args) {
+        String s = "gopi";
+        for(int i = 0;i < s.length(); i++){
+            for(int j = i+1; j<=s.length(); j++){
+                System.out.print(s.substring(i,j) +" ");
+            }
+            System.out.println();
+        }
+    }
+}
