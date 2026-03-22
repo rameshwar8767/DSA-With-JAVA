b4e8c21f24a700edec4fc25b4ab150a78f657ad1
rameshwar mane <manerameshwar909@gmail.com>
2026-03-22 14:15:19 +0530

    Fix snake pattern traversal logic for matrix printing

---
+++ 2DArray/PrintMatrixInSnakePattern.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class PrintMatrixInSnakePattern {
+
+    public static void printMatrixInSnakePattern(int[][] matrix){
+
+        for(int i = 0; i < matrix.length; i++){
+
+            // even row → left to right
+            if(i % 2 == 0){
+                for(int j = 0; j < matrix[0].length; j++){
+                    System.out.print(matrix[i][j] + " ");
+                }
+            }
+            // odd row → right to left
+            else{
+                for(int j = matrix[0].length - 1; j >= 0; j--){
+                    System.out.print(matrix[i][j] + " ");
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1,2,3,4,5},
+            {6,2,-1,-5,3},
+            {3,2,1,0,4},
+            {-2,1,3,-4,5}
+        };
+
+        printMatrixInSnakePattern(matrix);
+    }
+}
