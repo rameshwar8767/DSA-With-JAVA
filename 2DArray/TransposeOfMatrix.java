13fd9025828b47ca8bffee8e009d9cfaedc07c41
rameshwar mane <manerameshwar909@gmail.com>
2026-04-03 20:38:37 +0530

    Add TransposeOfMatrix class with methods for transposing and printing a matrix

---
+++ 2DArray/TransposeOfMatrix.java
@@ -0,0 +1,38 @@
+public class TransposeOfMatrix {
+
+    public static int[][] transpose(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] result = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+
+    // Helper method to print matrix
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        int[][] result = transpose(matrix);
+
+        printMatrix(result);
+    }
+}
--- OOPs/Inheritence.java
+++ OOPs/Inheritence.java
@@ -1,3 +1,7 @@
+import java.util.*;
+
 public class Inheritence {
-    
+    public static void main(String[] args) {
+        
+    }
 }
