9d846faea4ff10840d462f964b445a09515c7ca3
rameshwar mane <manerameshwar909@gmail.com>
2026-04-07 21:55:54 +0530

    Add Inheritence class demonstrating basic inheritance with Animal and Dog classes

--- OOPs/Inheritence.java
+++ OOPs/Inheritence.java
@@ -1,7 +1,25 @@
 import java.util.*;
 
+// Parent class
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating");
+    }
+}
+
+// Child class (inherits Animal)
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Dog is barking");
+    }
+}
+
 public class Inheritence {
     public static void main(String[] args) {
         
+        Dog d = new Dog();
+
+        d.eat();   // inherited method
+        d.bark();  // own method
     }
 }
