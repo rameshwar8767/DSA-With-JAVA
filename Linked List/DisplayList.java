7fde9ebdad93573c76caf55e8ea735568e53898c
rameshwar mane <manerameshwar909@gmail.com>
2026-04-08 13:13:32 +0530

    Add DisplayList class to demonstrate linked list display functionality

---
+++ Linked List/DisplayList.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Node{
+    int val;
+    Node next;
+
+    Node(int val){
+        this.val = val;
+    }
+}
+
+public class DisplayList {
+
+    public static void display(Node head){
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.print("null");
+        
+    }
+    public static void main(String[] args) {
+        // 10 -> 20 -> 30 -> 40 -> 50
+        Node a = new Node(10);    // a.val = 10;
+        Node b = new Node(20);    // b.val = 20;
+        Node c = new Node(30);    // c.val = 30;
+        Node d = new Node(40);    // d.val = 40;
+        Node e = new Node(50);    // e.val = 50;
+        
+        // Connect Node(Link Karenge);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = e;
+        e.next = null;
+        display(a);
+    }
+}
