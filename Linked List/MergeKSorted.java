1150e282f0c06d101c4723b15610ec391c7121db
rameshwar mane <manerameshwar909@gmail.com>
2026-05-04 22:29:40 +0530

    Add MergeKSorted class to implement merging of k sorted linked lists

---
+++ Linked List/MergeKSorted.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class ListNode{
+    ListNode next;
+    int val;
+
+    ListNode(int val){
+        this.val = val;
+    }
+}
+
+public class MergeKSorted {
+
+    public static ListNode mergeKLists(ListNode[] lists){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
