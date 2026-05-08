94c9990e3be644c05ed1450f773be75c34890bad
rameshwar mane <manerameshwar909@gmail.com>
2026-05-08 11:39:47 +0530

    Add CheckIfCircularLinkedList class to implement detection of circular linked lists

---
+++ Linked List/CheckIfCircularLinkedList.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Node {
+    int val;
+    Node next;
+
+    Node(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class CheckIfCircularLinkedList {
+
+    public static boolean isCircular(Node head) {
+
+        if (head == null) return false;
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        // Creating nodes
+        Node head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+
+        // Linking nodes
+        head.next = second;
+        second.next = third;
+
+        // Make circular
+        third.next = head;
+
+        System.out.println(isCircular(head)); // true
+    }
+}
