f84dfbabbc689b15050405e0d9a2cc3b920bf8b9
rameshwar mane <manerameshwar909@gmail.com>
2026-05-21 09:25:33 +0530

    Add Distinct class to implement counting unique elements in an array

---
+++ HashSet/Distinct.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Distinct {
+
+    public static int distinct(int arr[]){
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i = 0; i < arr.length; i++){
+            set.add(arr[i]);
+        }
+        
+        return set.size();
+    }
+    public static void main(String[] args) {
+        int[] arr = {2,2,3,2};
+        System.out.println(distinct(arr));
+    }
+}
