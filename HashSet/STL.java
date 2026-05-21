53bf16deec8dd778e748b677b8aa734806d6488b
rameshwar mane <manerameshwar909@gmail.com>
2026-05-21 09:20:34 +0530

    Add STL class to demonstrate usage of TreeSet for unique element storage

---
+++ HashSet/STL.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class STL{
+    public static void main(String[] args) {
+        // hashset contains unique element
+        // HashSet<Integer> set = new HashSet<>();
+        TreeSet<Integer> set = new TreeSet<>();
+        set.add(4);
+        set.add(24);
+        set.add(11);
+        set.add(10);
+        set.add(5);
+
+        System.out.println(set.size());
+        System.out.println(set.contains(24));
+        set.remove(24);
+        System.out.println(set.size());
+        System.out.println(set.contains(24));
+        System.out.println(set);
+    }
+}
