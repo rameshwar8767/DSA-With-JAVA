a18e42697d8acce40aedc17f336144086956e1b7
rameshwar mane <manerameshwar909@gmail.com>
2026-05-24 11:14:23 +0530

    feat: add CopyListWithRandomPointer class to implement a linked list with random pointers

---
+++ Linked List/CopyListWithRandomPointer.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class ListNode{
+    int val;
+    ListNode next;
+
+    ListNode(int val){
+        this.val = val;
+    }
+}
+
+public class CopyListWithRandomPointer {
+    // LeetCode 138
+    public static void main(String[] args) {
+        
+    }
+}
