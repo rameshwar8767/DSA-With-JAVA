0a79ed7e485e53073297e5fed96668528ac20265
rameshwar mane <manerameshwar909@gmail.com>
2026-06-29 09:46:35 +0530

    feat: add SumOfBinaryTree class to calculate the sum of a binary tree

---
+++ Binary Trees/SumOfBinaryTree.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+public class SumOfBinaryTree {
+    // GFG
+
+    public static int sumBT(Node root) {
+        if (root == null)
+            return 0;
+
+        return root.data + sumBT(root.left) + sumBT(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println("Sum = " + sumBT(root));
+    }
+}
