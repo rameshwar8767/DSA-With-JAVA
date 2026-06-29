735233372e6ac5349eda8c3d2e957a11e6dd6eed
rameshwar mane <manerameshwar909@gmail.com>
2026-06-29 09:36:21 +0530

    feat: add SizeOfBinaryTree class to calculate the size of a binary tree

---
+++ Binary Trees/SizeOfBinaryTree.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class SizeOfBinaryTree {
+    // GFG
+
+    public static int getSize(Node root) {
+        if (root == null)
+            return 0;
+
+        return 1 + getSize(root.left) + getSize(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println("Size of Binary Tree = " + getSize(root));
+    }
+}
