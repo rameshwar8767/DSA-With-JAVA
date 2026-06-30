1d4b2733700d18c921a6892c01c41b37e15deb80
rameshwar mane <manerameshwar909@gmail.com>
2026-06-30 10:34:14 +0530

    feat: add BinaryTreeLevelOrderTraversal class to implement level order traversal of a binary tree

---
+++ Binary Trees/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class BinaryTreeLevelOrderTraversal {
+
+    //Leetcode 102
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if (root == null) return ans;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+                level.add(curr.val);
+
+                if (curr.left != null)
+                    q.add(curr.left);
+
+                if (curr.right != null)
+                    q.add(curr.right);
+            }
+
+            ans.add(level);
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println(levelOrder(root));
+    }
+}
